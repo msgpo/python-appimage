9ec4f5e0c36d9e668b32a5552131872f8fc22d62
anki-code <anki-code>
2020-04-21 21:51:02 +0300

    fix thread

--- applications/xonsh/entrypoint.sh
+++ applications/xonsh/entrypoint.sh
@@ -1,1 +1,2 @@
-{{ python-executable }} -u "${APPDIR}/opt/python{{ python-version }}/bin/xonsh" "$@"
+# THREAD_SUBPROCS=False - fix of https://github.com/xonsh/xonsh/issues/3525#issuecomment-617307862
+THREAD_SUBPROCS=False {{ python-executable }} -u "${APPDIR}/opt/python{{ python-version }}/bin/xonsh" "$@"
